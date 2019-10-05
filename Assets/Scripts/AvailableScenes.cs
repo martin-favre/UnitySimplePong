f69bcf1e8e04932060f1c1f54fe3f94800519dd5
kalsipp <martin.favre22@gmail.com>
2019-10-05 16:07:04 +0200

    Added main menu skeleton

---
+++ Assets/Scripts/AvailableScenes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AvailableScenes
+{
+    public const string MAIN_GAME_SCENE = "MainGameScene";
+    public const string MAIN_MENU_SCENE = "MainMenuScene";
+}
---
+++ Assets/Scripts/MainMenu/MainMenuButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonManager : MonoBehaviour
+{
+
+    public void OnStartButtonPressed()
+    {
+        SceneManager.LoadScene(AvailableScenes.MAIN_GAME_SCENE);
+    }
+
+    public void OnOptionsButtonPressed()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnExitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
