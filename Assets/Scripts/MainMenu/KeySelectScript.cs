25dfb5228bec00582c192e6f78dc1d319e45db9c
kalsipp <martin.favre22@gmail.com>
2019-10-05 17:06:43 +0200

    Added skeleton for options menu

---
+++ Assets/Scripts/MainMenu/KeySelectScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class KeySelectScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    private KeyCode myKeycode;
+    private static bool anyKeyBeingChanged = false;
+    private STATE state;
+    enum STATE
+    {
+        NOT_BEING_CHANGED,
+        CHANGING
+    }
+
+    private void Start ()
+    {
+        SetText("W"); //TODO, get info from non-volatile storage
+    }
+
+    void SetText(string newText)
+    {
+        if(text != null)
+        {
+            text.text = newText;
+        }
+        else
+        {
+            Debug.LogWarning(this.GetType().Name + "Did not have component TextMeshProUGUI");
+        }
+    }
+
+    void OnGUI ()
+    {
+        if (state == STATE.CHANGING)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                myKeycode = e.keyCode;
+                SetText(myKeycode.ToString());
+                state = STATE.NOT_BEING_CHANGED;
+                anyKeyBeingChanged = false;
+            }
+        }
+    }
+
+    public void OnKeyButtonPressed ()
+    {
+        if (!anyKeyBeingChanged && state != STATE.CHANGING)
+        {
+            anyKeyBeingChanged = true;
+            state = STATE.CHANGING;
+        }
+    }
+}
